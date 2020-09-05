cda095797ebe8e89d205183145a517f657086de9
ikcilrep <frajerzycki@protonmail.com>
2020-09-06 00:42:12 +0200

    Save recipes in cookies

--- src/Components/recipes/SwipeScreen.js
+++ src/Components/recipes/SwipeScreen.js
@@ -1,16 +1,27 @@
 import React, { useState } from "react";
 import { randomMeal } from '../../Helpers/Recipes';
+import Cookies from 'universal-cookie';
 
 
 
 const SwipeScreen = () => {
     const [meal, setMeal] = useState(randomMeal());
+    const cookies = new Cookies();
+
+    if (!(cookies.get('saved-recipes') instanceof Array)) {
+        cookies.set('saved-recipes', []);
+    }
+
 
     const onSwipeLeft = () => {
         setMeal(randomMeal());
     };
 
     const onSwipeRight = () => {
+        const cookie = cookies.get('saved-recipes');
+        if (!cookie.includes(meal.id)) {
+            cookies.set('saved-recipes', cookie.concat(meal.id));
+        }
         setMeal(randomMeal());
     }
 
