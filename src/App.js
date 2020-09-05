ba0a622ae82a24d5e236e17334fa21339fa04b53
ikcilrep <frajerzycki@protonmail.com>
2020-09-05 16:06:36 +0200

    Add LogoutButton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import LoginButton from './Components/LoginButton';
+import LogoutButton from './Components/LogoutButton';
 
 function App() {
   return (
     <div className="App">
-      <LoginButton/>
+      <LoginButton />
+      <LogoutButton />
     </div>
   );
 }
