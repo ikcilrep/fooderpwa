eaa0dfc1af05e7e480cf1aa28ef080a4610e1d3e
ikcilrep <frajerzycki@protonmail.com>
2020-09-05 23:59:18 +0200

    Add simple meal view

--- src/Components/recipes/SwipeScreen.js
+++ src/Components/recipes/SwipeScreen.js
@@ -1,8 +1,14 @@
 import React from "react";
+import randomMeal from '../../Helpers/Recipes';
 
 const SwipeScreen = () => {
+    const meal = randomMeal();
     return (
-        <h1>Swipe Screen</h1>
+        <>
+            <h1>{meal.name}</h1>
+            <img alt="Very taste image." src={meal.pictureAddress}/>
+            <p>{meal.description}</p>
+        </>
     );
 }
 
