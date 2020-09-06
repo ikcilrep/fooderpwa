d6c67716c436bcc7a7028bf005dd93bd960daa59
ikcilrep <frajerzycki@protonmail.com>
2020-09-06 13:16:48 +0200

    Add show more button

--- src/Components/recipes/SwipeScreen.js
+++ src/Components/recipes/SwipeScreen.js
@@ -6,6 +6,7 @@ import Cookies from 'universal-cookie';
 
 const SwipeScreen = () => {
     const [meal, setMeal] = useState(randomRecipe());
+    const [showMore, setShowMore] = useState(false);
     const cookies = new Cookies();
 
     if (!(cookies.get('saved-recipes') instanceof Array)) {
@@ -25,13 +26,27 @@ const SwipeScreen = () => {
         setMeal(randomRecipe());
     }
 
+    const onClickShowMore = () => {
+        setShowMore(!showMore);
+    };
+
     return (
         <>
             <button onClick={onSwipeLeft}>Left</button>
             <button onClick={onSwipeRight}>Right</button>
+            <button onClick={onClickShowMore}>{showMore ? 'Show less' : 'Show more'}</button>
+            {showMore ?
+                <>
+                    <h3>Ingredients:</h3>
+                    <ul>
+                        {meal.ingredients.map(ingredient => <li>{ingredient}</li>)}
+                    </ul>
+
+                    <p>{meal.description}</p>
+                </> : null}
             <h1>{meal.name}</h1>
             <img alt="Very tasty." src={meal.pictureAddress} />
-            <p>{meal.description}</p>
+
         </>
     );
 }
