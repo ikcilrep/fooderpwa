bbd4ecba51366876e00cfd9add8cdf61b91494e2
ikcilrep <frajerzycki@protonmail.com>
2020-09-06 13:39:20 +0200

    Fix subrouting

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import Recipe from "./Components/recipes/Recipe";
 
 import { useAuth0 } from "@auth0/auth0-react";
 import { meals } from "./Helpers/Recipes";
-
+import SwipeScreen from "./Components/recipes/SwipeScreen";
+import SavedRecipesScreen from "./Components/recipes/SavedRecipesScreen";
 
 const App = () => {
   const { isAuthenticated, isLoading } = useAuth0();
@@ -19,6 +20,12 @@ const App = () => {
 
   return (
     <Router>
+      <Route path="/swipe">
+        {isAuthenticated ? <SwipeScreen /> : <Redirect to="/auth" />}
+      </Route>
+      <Route path="/saved">
+        {isAuthenticated ? <SavedRecipesScreen /> : <Redirect to="/auth" />}
+      </Route>
       <Route path="/main">
         {isAuthenticated ? <MainScreen /> : <Redirect to="/auth" />}
       </Route>
--- src/Components/MainScreen.js
+++ src/Components/MainScreen.js
@@ -1,32 +1,20 @@
 import React from "react";
-import { Route, Switch, Link, useRouteMatch } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import SwipeScreen from "./recipes/SwipeScreen";
-import SavedRecipesScreen from "./recipes/SavedRecipesScreen";
 import LogoutButton from './auth/LogoutButton';
 
 // Displays recipe, allows swiping left or right
 const MainScreen = () => {
-    const { path, url } = useRouteMatch();
 
     return (
         <>
             <div>
                 <ul>
-                    <li><Link to={`${url}/swipe`}>Swipe</Link></li>
-                    <li><Link to={`${url}/saved`}>Saved</Link></li>
+                    <li><Link to={`/swipe`}>Swipe</Link></li>
+                    <li><Link to={`/saved`}>Saved</Link></li>
                 </ul>
             </div>
             <LogoutButton/>
-
-            <Switch>
-                <Route path={`${path}/swipe`}>
-                    <SwipeScreen />
-                </Route>
-                <Route path={`${path}/saved`}>
-                    <SavedRecipesScreen />
-                </Route>
-            </Switch>
         </>
     );
 }
